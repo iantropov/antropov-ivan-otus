0e4d64232d64190004fbf9eaee03252186a17e41
antropov-ivan <antropov.ivan@gmail.com>
2022-07-10 18:14:39 +0600

    Поправил число чисел

--- node/2-sorter/src/constants.js
+++ node/2-sorter/src/constants.js
@@ -2,5 +2,5 @@ import { absPath } from "./utils.js";
 
 export const INPUT_FILE_NAME = absPath("/data/input.txt");
 export const OUTPUT_FILE_NAME = absPath("/data/output.txt");
-export const NUMBERS_COUNT = 150;
+export const NUMBERS_COUNT = 15_000_000;
 export const NUMBERS_COUNT_IN_PRE_SORT_FILE = Math.floor(NUMBERS_COUNT / 10);
