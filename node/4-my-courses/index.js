9ebdea7c70aa126ea329ab1fd97390a630c86a6d
antropov-ivan <antropov.ivan@gmail.com>
2022-08-16 14:00:14 +0600

    Добавил работу с монгой

--- node/4-my-courses/index.js
+++ node/4-my-courses/index.js
@@ -1,10 +1,20 @@
 import express from "express";
 import addRequestId from "express-request-id";
 import morgan from "morgan";
+import { MongoClient } from 'mongodb';
 
 morgan.token("id", (req) => req.id.split("-")[0]);
 
 const app = express();
+let db = null;
+
+MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    db = client.db('otus');
+});
 
 app.set("view engine", "pug");
 
@@ -28,6 +38,31 @@ app.use(
 
 app.get("/", (req, res) => {
     res.render("index", { title: "Hello title", message: "Hello message" });
+});
+
+app.get("/courses", (req, res) => {
+    const cursor = db.collection('courses').find({}, {name: 1});
+    cursor.toArray((err, courses) => {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+        } else {
+            res.render("courses", { courses });
+        }
+    });
+});
+
+app.get("/courses/:name", (req, res) => {
+    db.collection('courses').findOne({name: req.params.name}, (err, course) => {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+        } else if (!course) {
+            res.sendStatus(404);
+        } else {
+            res.render("course", { course });
+        }
+    });
 });
 
 app.get("/test", (req, res) => {
