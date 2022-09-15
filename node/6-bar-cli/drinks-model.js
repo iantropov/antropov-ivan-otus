da60eb532b225215f8a9b327242f03b09794d142
antropov-ivan <antropov.ivan@gmail.com>
2022-09-15 13:03:00 +0600

    Добавил инициализацию при отсутствии файла

--- node/6-bar-cli/drinks-model.js
+++ node/6-bar-cli/drinks-model.js
@@ -1,8 +1,16 @@
-const { writeFileSync } = require("node:fs");
+const { readFileSync, writeFileSync } = require("node:fs");
 
 const DRINKS_PATH = "./data/drinks.json";
+const DEFAULT_DRINKS = [{"name":"cola","volume":0.5},{"name":"old-drink","volume":6}];
 
-const drinks = require(DRINKS_PATH);
+let drinks = null;
+try {
+    drinks = JSON.parse(readFileSync(DRINKS_PATH, 'utf-8'));
+} catch(error) {
+    if (error.code === 'ENOENT') {
+        drinks = DEFAULT_DRINKS;
+    }
+}
 
 module.exports = {
     saveDrinks() {
