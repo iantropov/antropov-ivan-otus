5ad86f1b09f3e3d835e376a3c01a612c2f103fc2
antropov-ivan <antropov.ivan@gmail.com>
2022-10-02 20:16:26 +0600

    NestJS HelloWorld + PostgreSQL

---
+++ node/7-nestjs-api/src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'postgres',
+            password: 'pass123',
+            database: 'postgres',
+            autoLoadEntities: true,
+            synchronize: true
+        })
+    ],
+    controllers: [AppController],
+    providers: [AppService]
+})
+export class AppModule {}
