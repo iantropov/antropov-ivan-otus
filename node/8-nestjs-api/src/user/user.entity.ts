74da192b19a77f515761fe6373fcb55d6c893e99
antropov-ivan <antropov.ivan@gmail.com>
2022-10-15 18:59:28 +0600

    Фиксы

--- node/8-nestjs-api/src/user/user.entity.ts
+++ node/8-nestjs-api/src/user/user.entity.ts
@@ -18,7 +18,7 @@ export class User {
     @Column()
     password: string;
 
-    @OneToMany(() => Message, message => message.user)
+    @OneToMany(() => Message, message => message.user, { eager: true })
     @JoinColumn()
-    messages: Message[];
+    messages?: Message[];
 }
--- node/8-nestjs-api/src/user/user.module.ts
+++ node/8-nestjs-api/src/user/user.module.ts
@@ -4,12 +4,11 @@ import { UserController } from './user.controller';
 import { User } from './user.entity';
 import { UserService } from './user.service';
 import { UserResolver } from './user.resolver';
-import { UserMessagesResolver } from './user-messages.resolver';
 
 @Module({
     imports: [TypeOrmModule.forFeature([User])],
     controllers: [UserController],
-    providers: [UserService, UserResolver, UserMessagesResolver],
+    providers: [UserService, UserResolver],
     exports: [UserService]
 })
 export class UserModule {}
