62fa03bcb57c606c50c9f14279f6d00b2b46bf21
antropov-ivan <antropov.ivan@gmail.com>
2022-10-29 10:38:04 +0600

    Отгадки заклинаний

--- algo/1-harry-potter/main.go
+++ algo/1-harry-potter/main.go
@@ -18,11 +18,31 @@ func outputSquareWithValue(width int, height int, getValue func(int, int) string
 }
 
 func main() {
-	outputSquareWithValue(20, 20, func(i, j int) string {
-		if i >= j {
-			return "*"
+	outputSquareWithValue(25, 25, func(x, y int) string {
+		if x >= 13-y && x < y+12 && y < 13+x {
+			return "#"
 		} else {
 			return "."
 		}
 	})
 }
+
+// 1 - x > y
+
+// 2 - x == y
+// 3 - x == 24 - y
+// 4 - x < 30 - y
+// 5 - x / 2 == y
+// 6 - x < 10 || y < 10
+// 7 - x > 15 && y > 15
+
+// 8 - x == 0 || y == 0
+// 9 - x > y + 10 || x < y - 10
+// 10 - x > y && x / 2 <= y
+// 11 - x == 1 || x == 23 || y == 1 || y == 23
+// 12 - ?? - circle ?
+// 13 - y > 19 - x && y < 29 - x
+// 14 - ??
+// 15 - x > y + 10 && x < y + 21 || x < y - 10 && x > y - 21
+// 16 -
+// 17 -
