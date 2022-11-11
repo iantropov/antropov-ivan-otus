bdaaee1fcb5de24458c4d7a9c11d2f7df9574007
antropov-ivan <antropov.ivan@gmail.com>
2022-11-11 10:15:19 +0600

    Поиск простых чисел перебором

--- algo/3-algebraic-algorithms/main.go
+++ algo/3-algebraic-algorithms/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"algebraic-algorithms/fibonacci"
 	"algebraic-algorithms/power"
+	"algebraic-algorithms/primes"
 	"fmt"
 )
 
@@ -11,4 +12,5 @@ func main() {
 	fmt.Printf("5 ^ 3 = %d\n", power.Iterative(5, 3))
 	fmt.Printf("Fibonnaci(5) (recursive) = %d\n", fibonacci.Recursive(5))
 	fmt.Printf("Fibonnaci(5) (iterative) = %d\n", fibonacci.Iterative(5))
+	fmt.Printf("5 first primers (brute-force) = %v\n", primes.BruteForce(5))
 }
---
+++ algo/3-algebraic-algorithms/primes/brute-force.go
@@ -0,0 +1,22 @@
+package primes
+
+func BruteForce(num int) []int {
+	primes := make([]int, 0)
+	for i := 1; len(primes) < num; i++ {
+		if isPrime(i) {
+			primes = append(primes, i)
+		}
+	}
+
+	return primes
+}
+
+func isPrime(num int) bool {
+	for i := 2; i < num; i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+
+	return true
+}
