f6b10d2abee8421afa1e6fa46dbba85272dfce6d
antropov-ivan <antropov.ivan@gmail.com>
2022-11-12 17:28:24 +0600

    Биты только для нечётных чисел

--- algo/3-algebraic-algorithms/primes/eratosthenes-with-bits.go
+++ algo/3-algebraic-algorithms/primes/eratosthenes-with-bits.go
@@ -1,25 +1,26 @@
 package primes
 
 func EratosthenesWithBits(num int) int {
-	lengthInInt32 := num / 32
-	if num%32 > 0 {
+	correctedN := num / 2
+	lengthInInt32 := correctedN / 32
+	if correctedN%32 > 0 {
 		lengthInInt32++
 	}
 	notPrimers := make([]int32, lengthInInt32)
 
-	for i := 2; i < num; i++ {
-		if isNotPrimer(i, notPrimers) == false {
+	for i := 3; i < num; i++ {
+		if i%2 == 1 && isNotPrimer(i, notPrimers) == false {
 			markNotPrimersWithBits(i, num, notPrimers)
 		}
 	}
 
 	count := 0
-	for i := 2; i < num; i++ {
-		if isNotPrimer(i, notPrimers) == false {
+	for i := 3; i < num; i++ {
+		if i%2 == 1 && isNotPrimer(i, notPrimers) == false {
 			count++
 		}
 	}
-	return count
+	return count + 1 // add '2'
 }
 
 func markNotPrimersWithBits(n, num int, notPrimers []int32) {
@@ -33,14 +34,16 @@ func markNotPrimersWithBits(n, num int, notPrimers []int32) {
 }
 
 func isNotPrimer(n int, notPrimers []int32) bool {
-	i := n / 32
-	j := n % 32
+	correctedN := n/2 - 1
+	i := correctedN / 32
+	j := correctedN % 32
 	mask := int32(01 << j)
 	return (notPrimers[i] & mask) == mask
 }
 
 func markAsNotPrimer(n int, notPrimers []int32) {
-	i := n / 32
-	j := n % 32
+	correctedN := n/2 - 1
+	i := correctedN / 32
+	j := correctedN % 32
 	notPrimers[i] |= (01 << j)
 }
