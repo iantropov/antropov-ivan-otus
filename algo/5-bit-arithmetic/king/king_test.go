4cafe857a3fd3dccd7208b307b412e3ad8608171
antropov-ivan <antropov.ivan@gmail.com>
2022-11-17 11:00:09 +0600

    Размещение короля (WIP)

---
+++ algo/5-bit-arithmetic/king/king_test.go
@@ -0,0 +1,11 @@
+package king
+
+const KING_TEST_DIR = "/Users/antropov-ivan/Downloads/0.BITS/1.Bitboard - Король/"
+
+// func TestKingWithCountBitsStraightForward(t *testing.T) {
+// 	chessPiece.TestChessPiece(KING_TEST_DIR, "king/count-bits-straight-forward", PlaceKing, countBits.CountBitsStraightforward, t)
+// }
+
+// func TestKingWithCountBitsBySubstraction(t *testing.T) {
+// 	chessPiece.TestChessPiece(KING_TEST_DIR, "king/count-bits-straight-forward", PlaceKing, countBits.CountBitsBySubstraction, t)
+// }
