879bb1e2bea8cf40e6e14ceb24353f338cd91902
antropov-ivan <antropov.ivan@gmail.com>
2022-11-24 10:47:18 +0600

    Замеры производительности сортировок

--- algo/7-heapsort/main.go
+++ algo/7-heapsort/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"heapsort/heap"
+	"heapsort/selection"
+	"heapsort/sorting"
+	"math/rand"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello, the seventh homework!")
+	fmt.Println("Hello from seventh homework!")
+
+	N := 1_000_000
+	numbers := make([]int, N)
+	for i := range numbers {
+		numbers[i] = i
+	}
+
+	rand.Shuffle(N, func(i, j int) { numbers[i], numbers[j] = numbers[j], numbers[i] })
+
+	fmt.Println("Sorting of a shuffled array of length ", N)
+
+	measureSorting("#selection", selection.Sort, numbers)
+	measureSorting("#heap", heap.SortHeapify, numbers)
+}
+
+func measureSorting(name string, sort sorting.Sort, numbers []int) {
+	start := time.Now()
+
+	sorted := sort(numbers)
+
+	elapsed := time.Since(start)
+	fmt.Println("Sorting Time for "+name, elapsed)
+
+	for i := 0; i < len(numbers); i++ {
+		if sorted[i] != i {
+			fmt.Println("FAIL")
+		}
+	}
 }
