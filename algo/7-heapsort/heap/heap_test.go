4e3e87bf5f12983974d8c526331f8e1dcf141f38
antropov-ivan <antropov.ivan@gmail.com>
2022-11-26 10:41:02 +0600

    Добавил тестирование набором тестов

--- algo/7-heapsort/heap/heap_test.go
+++ algo/7-heapsort/heap/heap_test.go
@@ -12,3 +12,19 @@ func TestHeapSwimSink(t *testing.T) {
 func TestHeapHeapify(t *testing.T) {
 	sorting.TestSorting(SortHeapify, t)
 }
+
+func TestHeapifyWithRandomFiles(t *testing.T) {
+	sorting.TestSortingWithRandomFiles(SortHeapify, t)
+}
+
+func TestHeapifyWithDigitsFiles(t *testing.T) {
+	sorting.TestSortingWithDigitsFiles(SortHeapify, t)
+}
+
+func TestHeapifyWithSortedFiles(t *testing.T) {
+	sorting.TestSortingWithSortedFiles(SortHeapify, t)
+}
+
+func TestHeapifyWithReversFiles(t *testing.T) {
+	sorting.TestSortingWithReversFiles(SortHeapify, t)
+}
--- algo/7-heapsort/selection/selection_test.go
+++ algo/7-heapsort/selection/selection_test.go
@@ -8,3 +8,19 @@ import (
 func TestSelection(t *testing.T) {
 	sorting.TestSorting(Sort, t)
 }
+
+func TestSelectionWithRandomFiles(t *testing.T) {
+	sorting.TestSortingWithRandomFiles(Sort, t)
+}
+
+func TestSelectionWithDigitsFiles(t *testing.T) {
+	sorting.TestSortingWithDigitsFiles(Sort, t)
+}
+
+func TestSelectionWithSortedFiles(t *testing.T) {
+	sorting.TestSortingWithSortedFiles(Sort, t)
+}
+
+func TestSelectionWithReversFiles(t *testing.T) {
+	sorting.TestSortingWithReversFiles(Sort, t)
+}
