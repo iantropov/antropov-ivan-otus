5244914778e488fc058c7aff70fb4ba3fc579ad7
antropov-ivan <antropov.ivan@gmail.com>
2022-11-27 10:05:27 +0600

    Раскомментировал тесты

--- algo/8-quicksort-mergesort/main.go
+++ algo/8-quicksort-mergesort/main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"quicksort-mergesort/external"
+	"quicksort-mergesort/merge"
+	"quicksort-mergesort/quick"
 	"quicksort-mergesort/sorting"
 	"time"
 )
@@ -11,22 +12,18 @@ import (
 func main() {
 	fmt.Println("Hello from eighth homework!")
 
-	rand.Seed(time.Now().UnixNano())
-	external.GenerateTextFile("input.txt", 10, 100)
-	external.SortBySplit("input.txt", "output.txt", 10, 3)
+	N := 10_000_000
+	numbers := make([]int, N)
+	for i := range numbers {
+		numbers[i] = i
+	}
 
-	// N := 10_000_000
-	// numbers := make([]int, N)
-	// for i := range numbers {
-	// 	numbers[i] = i
-	// }
+	rand.Shuffle(N, func(i, j int) { numbers[i], numbers[j] = numbers[j], numbers[i] })
 
-	// rand.Shuffle(N, func(i, j int) { numbers[i], numbers[j] = numbers[j], numbers[i] })
+	fmt.Println("Sorting of a shuffled array of length ", N)
 
-	// fmt.Println("Sorting of a shuffled array of length ", N)
-
-	// measureSorting("#quick", quick.Sort, numbers)
-	// measureSorting("#merge", merge.Sort, numbers)
+	measureSorting("#quick", quick.Sort, numbers)
+	measureSorting("#merge", merge.Sort, numbers)
 }
 
 func measureSorting(name string, sort sorting.Sort, numbers []int) {
