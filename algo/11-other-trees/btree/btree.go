e273b6d6a38ba4bd37c657ee29cd20d4f6c14994
antropov-ivan <antropov.ivan@gmail.com>
2022-12-18 11:19:05 +0600

    Удаление из B дерева (WIP)

--- algo/11-other-trees/btree/btree.go
+++ algo/11-other-trees/btree/btree.go
@@ -7,7 +7,7 @@ type node struct {
 	pointers []*node
 	size     int
 	leaf     bool
-	t        int // minimal degree, t >= 2
+	tree     *btree
 }
 
 type btree struct {
@@ -16,6 +16,9 @@ type btree struct {
 }
 
 func NewTree(t int) *btree {
+	if t < 2 {
+		panic("Minimal degree of B tree should be >= 2")
+	}
 	return &btree{nil, t}
 }
 
@@ -26,14 +29,14 @@ func (tree *btree) Search(val int) bool {
 
 func (tree *btree) Insert(val int) {
 	if tree.root == nil {
-		tree.root = buildNode(tree.t)
+		tree.root = tree.buildNode()
 		tree.root.insertIntoNode(val, nil)
 		tree.root.leaf = true
 		return
 	}
 
 	if tree.root.isFull() {
-		newRoot := buildNode(tree.t)
+		newRoot := tree.buildNode()
 		newRoot.pointers[0] = tree.root
 
 		newVal, newRightChild := tree.root.split()
@@ -43,6 +46,15 @@ func (tree *btree) Insert(val int) {
 	}
 
 	tree.root.insert(val)
+}
+
+func (tree *btree) Remove(val int) {
+	node := tree.root.searchNode(val)
+	if node == nil {
+		return
+	}
+
+	tree.root.remove(val)
 }
 
 func (tree *btree) dump() {
@@ -118,7 +130,7 @@ func (n *node) findChild(val int) *node {
 }
 
 func (n *node) split() (int, *node) {
-	if n.size < n.t-1 {
+	if n.size < n.tree.t-1 {
 		panic("the leaf is too sparse")
 	}
 
@@ -127,7 +139,7 @@ func (n *node) split() (int, *node) {
 	newVal := n.values[pos]
 	n.values[pos] = 0
 
-	newRightChild := buildNode(n.t)
+	newRightChild := n.tree.buildNode()
 	newRightChild.pointers[0] = n.pointers[pos+1]
 	n.pointers[pos+1] = nil
 	newRightChild.leaf = n.leaf
@@ -145,8 +157,114 @@ func (n *node) split() (int, *node) {
 	return newVal, newRightChild
 }
 
+func (n *node) remove(val int) {
+	if n.leaf {
+		n.removeFromNode(val)
+		return
+	}
+
+	if n.contains(val) {
+		pos := n.findPos(val)
+		child := n.pointers[pos]
+		nextChild := n.pointers[pos+1]
+		if child.size >= n.tree.t {
+			n.values[pos] = child.removePredecessor(val)
+		} else if nextChild.size >= n.tree.t {
+			n.values[pos] = nextChild.removeSuccessor(val)
+		} else {
+			child.merge(n.values[pos+1], nextChild)
+			n.removeFromNode(val)
+		}
+		return
+	}
+
+	pos := n.findPos(val)
+	child := n.pointers[pos]
+	leftChild := n.pointers[pos-1]
+	rightChild := n.pointers[pos+1]
+	if child.size < n.tree.t {
+		if leftChild.size < n.tree.t {
+			if rightChild.size < n.tree.t {
+				valueToMoveDown := n.values[pos]
+				n.removeFromNode(valueToMoveDown)
+				leftChild.merge(valueToMoveDown, child)
+				leftChild.remove(val)
+				return
+			} else {
+				child.insertIntoNode(rightChild.values[0], rightChild.pointers[0])
+				rightChild.removeFromNode(rightChild.values[0])
+				child.remove(val)
+				return
+			}
+		} else {
+			child.insertIntoNode(leftChild.values[leftChild.size-1], leftChild.pointers[leftChild.size])
+			leftChild.removeFromNode(leftChild.values[leftChild.size-1])
+			child.remove(val)
+			return
+		}
+	} else {
+		child.remove(val)
+	}
+}
+
+// TODO
+func (n *node) findPos(val int) int {
+	pos := 0
+	for ; pos < n.size && n.values[pos] < val; pos++ {
+	}
+	return pos
+}
+
+func (n *node) removeFromNode(val int) {
+	if n.size < n.tree.t {
+		panic("the leaf is too sparse")
+	}
+
+	pos := 0
+	for ; pos < n.size && n.values[pos] < val; pos++ {
+	}
+
+	for i := pos; i < n.size; i++ {
+		n.values[i] = n.values[i+1]
+		n.pointers[i+1] = n.pointers[i+2]
+	}
+
+	n.size--
+}
+
+func (n *node) removePredecessor(val int) int {
+	return 0
+}
+
+func (n *node) removeSuccessor(val int) int {
+	return 0
+}
+
+func (n *node) merge(val int, sibling *node) {
+	if n.size+sibling.size+1 > 2*n.tree.t-1 {
+		panic("Too many values for merge")
+	}
+
+	n.values[n.size] = val
+	n.pointers[n.size+1] = sibling.pointers[0]
+	for i, value := range sibling.values {
+		n.values[i+n.size+1] = value
+		n.pointers[i+n.size+2] = sibling.pointers[i+1]
+	}
+	n.size += sibling.size + 1
+}
+
 func (n *node) isFull() bool {
 	return n.size == len(n.values)
+}
+
+func (n *node) contains(val int) bool {
+	for i := 0; i < n.size; i++ {
+		if n.values[i] == val {
+			return true
+		}
+	}
+	return false
 }
 
 func (n *node) dump() {
@@ -160,10 +278,10 @@ func (n *node) dump() {
 	}
 }
 
-func buildNode(t int) *node {
+func (tree *btree) buildNode() *node {
 	newNode := &node{}
-	newNode.values = make([]int, 2*t-1)
-	newNode.pointers = make([]*node, 2*t)
-	newNode.t = t
+	newNode.values = make([]int, 2*tree.t-1)
+	newNode.pointers = make([]*node, 2*tree.t)
+	newNode.tree = tree
 	return newNode
 }
