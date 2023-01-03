42cbe17157be7d859636d9c7c73228944fa1813a
antropov-ivan <antropov.ivan@gmail.com>
2023-01-03 09:41:10 +0600

    Реализовал trie на списке

--- algo/15-prefix-tree/trie/trie.go
+++ algo/15-prefix-tree/trie/trie.go
@@ -1,14 +1,18 @@
 package trie
 
-const ASCII_BASE = 97
-
 type Trie struct {
 	root *node
 }
 
 type node struct {
-	chars [26]*node
-	end   bool
+	char *nodeChar
+	end  bool
+}
+
+type nodeChar struct {
+	c        rune
+	nextChar *nodeChar
+	nextNode *node
 }
 
 func Constructor() Trie {
@@ -19,10 +23,38 @@ func Constructor() Trie {
 func (this *Trie) Insert(word string) {
 	n := this.root
 	for _, c := range word {
-		if n.chars[c-ASCII_BASE] == nil {
-			n.chars[c-ASCII_BASE] = new(node)
+		nc := n.char
+		if nc == nil {
+			n.char = new(nodeChar)
+			n.char.c = c
+			nc = n.char
+		} else {
+			prev := (*nodeChar)(nil)
+			for ; nc != nil && nc.c < c; nc = nc.nextChar {
+				prev = nc
+			}
+			if nc == nil {
+				newNc := new(nodeChar)
+				newNc.c = c
+				prev.nextChar = newNc
+				nc = newNc
+			} else if nc.c != c {
+				newNc := new(nodeChar)
+				newNc.c = c
+				newNc.nextChar = nc
+				if prev != nil {
+					prev.nextChar = newNc
+				} else {
+					newNc.nextChar = n.char
+					n.char = newNc
+				}
+				nc = newNc
+			}
 		}
-		n = n.chars[c-ASCII_BASE]
+		if nc.nextNode == nil {
+			nc.nextNode = new(node)
+		}
+		n = nc.nextNode
 	}
 	n.end = true
 }
@@ -39,7 +71,20 @@ func (this *Trie) StartsWith(prefix string) bool {
 func (this *Trie) follow(s string) *node {
 	n := this.root
 	for _, c := range s {
-		n = n.chars[c-ASCII_BASE]
+		nodeChar := n.char
+		if n.char == nil {
+			return nil
+		}
+
+		for ; nodeChar.nextChar != nil; nodeChar = nodeChar.nextChar {
+			if nodeChar.c == c {
+				break
+			}
+		}
+		if nodeChar.c != c {
+			return nil
+		}
+		n = nodeChar.nextNode
 		if n == nil {
 			return nil
 		}
--- algo/15-prefix-tree/trie/trie_test.go
+++ algo/15-prefix-tree/trie/trie_test.go
@@ -25,3 +25,61 @@ func TestTrie(t *testing.T) {
 	require.False(t, trie.Search("asd"))
 	require.False(t, trie.StartsWith("asd"))
 }
+
+func TestTrie2(t *testing.T) {
+	trie := trie.Constructor()
+	trie.Insert("apple")
+	require.True(t, trie.Search("apple"))
+
+	trie.Insert("app")
+	require.True(t, trie.Search("app"))
+}
+
+func TestTrie3(t *testing.T) {
+	trie := trie.Constructor()
+	require.False(t, trie.Search("apple"))
+}
+
+func TestTrie4(t *testing.T) {
+	trie := trie.Constructor()
+
+	trie.Insert("hello")
+	require.False(t, trie.Search("hell"))
+	require.False(t, trie.Search("helloa"))
+	require.True(t, trie.Search("hello"))
+
+	require.True(t, trie.StartsWith("hell"))
+	require.False(t, trie.StartsWith("helloa"))
+	require.True(t, trie.StartsWith("hell"))
+}
+
+func TestTrie5(t *testing.T) {
+	trie := trie.Constructor()
+
+	trie.Insert("app")
+	trie.Insert("apple")
+	trie.Insert("beer")
+	trie.Insert("add")
+	trie.Insert("jam")
+	trie.Insert("rental")
+
+	require.False(t, trie.Search("apps"))
+	require.True(t, trie.Search("app"))
+	require.False(t, trie.Search("ad"))
+	require.False(t, trie.Search("applepie"))
+	require.False(t, trie.Search("rest"))
+	require.False(t, trie.Search("jan"))
+	require.False(t, trie.Search("rent"))
+	require.True(t, trie.Search("beer"))
+	require.True(t, trie.Search("jam"))
+
+	require.False(t, trie.StartsWith("apps"))
+	require.True(t, trie.StartsWith("app"))
+	require.True(t, trie.StartsWith("ad"))
+	require.False(t, trie.StartsWith("applepie"))
+	require.False(t, trie.StartsWith("rest"))
+	require.False(t, trie.StartsWith("jan"))
+	require.True(t, trie.StartsWith("rent"))
+	require.True(t, trie.StartsWith("beer"))
+	require.True(t, trie.StartsWith("jam"))
+}
