ce1ed1985408f0abf6c32f4846d5b8d7202d8898
antropov-ivan <antropov.ivan@gmail.com>
2023-01-04 08:32:34 +0600

    Обработка http запросов

--- highload/1-social-network-blank/main.go
+++ highload/1-social-network-blank/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const PORT = ":3000"
 
 func main() {
 	fmt.Println("Hello from the social network!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/hello", handleHello)
+	mux.HandleFunc("/bye", handleBye)
+
+	fmt.Println("Will serve on port", PORT)
+	err := http.ListenAndServe(PORT, mux)
+	log.Fatal(err)
+}
+
+func handleHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received request: Hello")
+	w.Write([]byte("Hello"))
+}
+
+func handleBye(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received request: Bye")
+	w.Write([]byte("Bye"))
 }
