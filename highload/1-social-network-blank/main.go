12a9a99d01af49551be864a241c0762cbe211e63
antropov-ivan <antropov.ivan@gmail.com>
2023-01-04 09:08:29 +0600

    Обработка json запроса

--- highload/1-social-network-blank/main.go
+++ highload/1-social-network-blank/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,14 +9,43 @@ import (
 
 const PORT = ":3000"
 
+type people struct {
+	Number int `json:"number"`
+}
+
+type UserParams struct {
+	FirstName  string `json:"first_name"`
+	SecondName string `json:"second_name"`
+	Age        int    `json:"age"`
+	Biography  string `json:"biography"`
+	City       string `json:"city"`
+	Password   string `json:"password"`
+}
+
+type UserResponse struct {
+	UserId string `json:"user_id"`
+}
+
 func main() {
+	text := `{"people": [{"craft": "ISS", "name": "Sergey Rizhikov"}, {"craft": "ISS", "name": "Andrey Borisenko"}, {"craft": "ISS", "name": "Shane Kimbrough"}, {"craft": "ISS", "name": "Oleg Novitskiy"}, {"craft": "ISS", "name": "Thomas Pesquet"}, {"craft": "ISS", "name": "Peggy Whitson"}], "message": "success", "number": 6}`
+	textBytes := []byte(text)
+
+	people1 := people{}
+	err := json.Unmarshal(textBytes, &people1)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(people1.Number)
+
 	fmt.Println("Hello from the social network!")
 	mux := http.NewServeMux()
 	mux.HandleFunc("/hello", handleHello)
 	mux.HandleFunc("/bye", handleBye)
+	mux.HandleFunc("/user/register", handleUserRegister)
 
 	fmt.Println("Will serve on port", PORT)
-	err := http.ListenAndServe(PORT, mux)
+	err = http.ListenAndServe(PORT, mux)
 	log.Fatal(err)
 }
 
@@ -27,4 +57,19 @@ func handleHello(w http.ResponseWriter, r *http.Request) {
 func handleBye(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Received request: Bye")
 	w.Write([]byte("Bye"))
+}
+
+func handleUserRegister(w http.ResponseWriter, r *http.Request) {
+	var params UserParams
+	err := json.NewDecoder(r.Body).Decode(&params)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	fmt.Println("Received User with params:", params)
+
+	userResponse := UserResponse{"e4d2e6b0-cde2-42c5-aac3-0b8316f21e58"}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(userResponse)
 }
