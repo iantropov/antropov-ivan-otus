d2916ced3825f4f3dbfd3d49c7b8d7ef8440a49b
antropov-ivan <antropov.ivan@gmail.com>
2023-01-15 09:46:48 +0600

    Реализовал алгоритм Дейкстры

---
+++ algo/17-minimal-tree/dijkstra/dijkstra_test.go
@@ -0,0 +1,20 @@
+package dijkstra_test
+
+import (
+	"minimal-tree/dijkstra"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestDijkstra(t *testing.T) {
+	path := dijkstra.Dijkstra([][]int{
+		0: {0, 1, 0, 3, 0},
+		1: {0, 0, 3, 1, 2},
+		2: {0, 0, 0, 0, 0},
+		3: {0, 0, 3, 0, 1},
+		4: {0, 0, 2, 0, 0},
+	}, 0, 4)
+
+	require.Equal(t, []int{0, 1, 4}, path)
+}
