83ceac1266b46e57a465223a0d2fd0f5dd74cb96
antropov-ivan <antropov.ivan@gmail.com>
2023-02-03 08:57:37 +0600

    Реализовал конечный автомат

--- algo/19-kmp/fsm/fsm.go
+++ algo/19-kmp/fsm/fsm.go
@@ -1,21 +1,29 @@
 package fsm
 
 // handles only A-Z
-const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var alphabet = []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 func SearchSubstring(text, pattern string) int {
+	delta := buildDelta([]byte(pattern))
+	patternIdx := 0
+	for i, t := range []byte(text) {
+		patternIdx = delta[patternIdx][t-alphabet[0]]
+		if patternIdx == len(pattern) {
+			return i - len(pattern) + 1
+		}
+	}
 	return -1
 }
 
 func buildDelta(pattern []byte) [][]int {
 	delta := make([][]int, len(pattern))
-	alphabet := []byte(ALPHABET)
 	for i := 0; i < len(pattern); i++ {
 		delta[i] = make([]int, len(alphabet))
 		for j := 0; j < len(alphabet); j++ {
-			line := append(pattern[:i], alphabet[j])
+			line := append([]byte(nil), pattern[:i]...)
+			line = append(line, alphabet[j])
 			k := i + 1
-			for !areEqualPrefixAndSuffix(pattern, line, k) {
+			for k > 0 && !areEqualPrefixAndSuffix(pattern, line, k) {
 				k--
 			}
 			delta[i][j] = k
@@ -25,5 +33,10 @@ func buildDelta(pattern []byte) [][]int {
 }
 
 func areEqualPrefixAndSuffix(left, right []byte, k int) bool {
-	return false
+	for i := 0; i < k; i++ {
+		if left[i] != right[len(right)-k+i] {
+			return false
+		}
+	}
+	return true
 }
