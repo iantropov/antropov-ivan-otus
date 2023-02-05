72aa4b43d5a4320fa16039f86efe2cdfc88f6ec5
antropov-ivan <antropov.ivan@gmail.com>
2023-02-05 08:23:35 +0600

    Реализовал алгоритм RLE

--- algo/20-rle/main.go
+++ algo/20-rle/main.go
@@ -4,4 +4,28 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello from the twentieth homework!")
+	var input string
+	fmt.Scan(&input)
+
+	output := rle([]byte(input))
+	fmt.Println(output)
+}
+
+func rle(input []byte) []byte {
+	res := make([]byte, 0)
+	if len(input) == 0 {
+		return res
+	}
+
+	count := 1
+	for i := 1; i < len(input); i++ {
+		if count == 255 || input[i] != input[i-1] {
+			res = append(res, byte(count), input[i-1])
+			count = 1
+		} else {
+			count++
+		}
+	}
+	res = append(res, byte(count), input[len(input)-1])
+	return res
 }
